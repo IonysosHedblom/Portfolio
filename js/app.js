ff4151565f3a970ec0bf9b8b4962adba72bbbe2f
Ionhedblom <ionysoshedblom@gmail.com>
2020-05-03 14:34:54 +0200

    Animation works

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 import { barSlide, titleSlide } from './gsap.js';
-
 const gsapSlides = () => {
   barSlide();
   titleSlide();
 };
 
-gsapSlides();
+// Projects animation on scroll
+const projects = $('.projects');
+
+$(window).scroll(() => {
+  const docViewTop = $(window).scrollTop();
+  const docViewBottom = docViewTop + $(window).height();
+  let executed = false;
+  const elemTop = $(projects).offset().top;
+  const elemBottom = elemTop + $(projects).height();
+  if (elemBottom <= docViewBottom && elemTop >= docViewTop && !executed) {
+    executed = true;
+    gsapSlides();
+    console.log('It works!');
+  }
+});
