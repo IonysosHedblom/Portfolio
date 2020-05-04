311a6eb857d12e9ac23c46ab507bc6d5d4ce50a1
Ionhedblom <ionysoshedblom@gmail.com>
2020-05-04 15:57:52 +0200

    Project grid added

--- js/gsap.js
+++ js/gsap.js
@@ -1,11 +1,11 @@
 export const barSlide = () => {
-  gsap.from('.header-bar', { duration: 1.5, x: 250, opacity: 0 });
-  gsap.to('.header-bar', { duration: 1.5, x: 0, opacity: 1 });
+  gsap.from('.header-bar', { duration: 1.6, x: 250, opacity: 0 });
+  gsap.to('.header-bar', { duration: 1.6, x: 0, opacity: 1 });
 };
 
 export const titleSlide = () => {
-  gsap.from('.project-title', { duration: 1.5, x: -250, opacity: 0 });
-  gsap.to('.project-title', { duration: 1.5, x: 0, opacity: 1 });
+  gsap.from('.project-header', { duration: 1.6, x: -250, opacity: 0 });
+  gsap.to('.project-header', { duration: 1.6, x: 0, opacity: 1 });
 };
 
 export const scrollTo = () => {
